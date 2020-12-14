73b2393798ff67c329bbc0ffb96406705e8c5ec7
User Friendly <userfriendly@foobar.ninja>
2020-12-14 18:52:41 +0200

    Making progress on the sequence UI element
    see #3

---
+++ Assets/Source/UI/Sequence.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PandoraCube.UI
+{
+
+    public class Sequence : MonoBehaviour
+    {
+        public Color active_color = new Color();
+        public GameObject[] icons = new GameObject[3];
+        public Sprite[] icon_set = new Sprite[6];
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Image icon1 = icons[0].transform.Find("Thumbnail").GetComponent<Image>();
+            //icon1.color = active_color;
+            //icon1.sprite = icon_set[5];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
